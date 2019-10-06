05521980b20ef330a302f29468c8f0da9763d94f
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-06 21:43:20 +0300

    Creates an empty list of credentials

--- passwords.py
+++ passwords.py
@@ -2,12 +2,13 @@ import unittest
 
 class Passwords :
     '''
-    class that creates instnces of users accounts passwords
+    class that creates instances of users accounts credentials
     '''
 
     credentials_list = [] #creates an empty list for user credentials
-    
-    def __init__(self, first_name, last_name, user_credentials):
+
+    def __init__(self, account_name, first_name, last_name, user_credentials):
+        self.account_name
         self.first_name = first_name
         self.last_name = last_name
         self.user_credentials = user_credentials
