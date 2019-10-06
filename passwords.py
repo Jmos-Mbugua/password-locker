8e2f7cbe39595612045a894122ec0ed985124179
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-06 22:15:00 +0300

    Adds a method which creates instances of  user credentials

--- passwords.py
+++ passwords.py
@@ -1,16 +1,14 @@
-import unittest
-
-class Passwords :
+class Password :
     '''
     class that creates instances of users accounts credentials
     '''
 
     credentials_list = [] #creates an empty list for user credentials
 
-    def __init__(self, account_name, first_name, last_name, user_credentials):
-        self.account_name
+    def __init__(self, account_name, first_name, last_name, user_password):
+        self.account_name = account_name
         self.first_name = first_name
         self.last_name = last_name
-        self.user_credentials = user_credentials
+        self.user_credentials = user_password
 
 
