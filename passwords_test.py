9a57473f7f8b290149a393d1bf7a90a247b40f39
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-06 22:14:03 +0300

    Creates a test class for testing Password class behaviours

---
+++ passwords_test.py
@@ -0,0 +1,10 @@
+import unittest
+from passwords import Password
+
+class TestPassword(unittest.TestCase):
+    '''
+    Test class that defines test cases for the Password class behaviours
+    '''
+
+    
+
