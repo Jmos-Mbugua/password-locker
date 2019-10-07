5eb3a44d3cdd51cb9181ad09ae882d9ade114bde
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-07 16:47:36 +0300

    Create a function that saves user login details when inputted

--- credentials.py
+++ credentials.py
@@ -53,7 +53,7 @@ class Password :
         self.user_password = user_password
 
 
-        
+
 
 
 
@@ -62,13 +62,30 @@ class User:
     class that creates instances of users
     '''
 
-    users_list = [] #creates an empty list for user credentials
+    users_list = [] #creates an empty list for us
 
 
-    def __init__(self, user_first_name, user_last_name, user_passwrd):
-        self.user_first_name = user_first_name
-        self.user_last_name = user_last_name
+    def __init__(self, user_name, user_passwrd):
+        self.user_name = user_name
         self.user_passwrd = user_passwrd
+
+    def save_users(self):
+        '''
+        This method saves new object to users list
+        '''
+        User.users_list.append(self)
+
+    @classmethod
+    def user_authenticate(cls, name, passwrd):
+        '''
+        This method checks whether the input details from the user match those in the user_list and returns boolean
+        '''
+        for user in cls.users_list:
+            if user.user_name == name and user.user_passwrd == passwrd:
+                return True
+        return False
+
+
 
 
 
