03c9d9952852c5e68203163ce35bdfedc415a40d
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-07 07:59:27 +0300

    Adds method to save multiple credentials as well as delete credential

--- passwords.py
+++ passwords.py
@@ -7,9 +7,25 @@ class Password :
 
     def save_credential(self):
         '''
-        Test Case to check if the credentials object is saved in the credential list
+        Method to save a new object in the credential list
         '''
         Password.credentials_list.append(self)
+
+    def delete_credential(self):
+        '''
+        Method to delete a credential from the list
+        '''
+        Password.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_account_name(cls, account_name):
+        '''
+        Method that takes in a site name and returns the credentials
+        '''
+        for credential in cls.credentials_list:
+            if credential.account_name == account_name:
+                return credential
+
 
 
     def __init__(self, account_name, first_name, last_name, user_password):
