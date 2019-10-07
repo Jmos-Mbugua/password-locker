67d22df5f7493b4743493bfc19ba6da9d37f83ef
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-07 04:59:29 +0300

    Adds method to save the user credentials

--- passwords.py
+++ passwords.py
@@ -5,10 +5,19 @@ class Password :
 
     credentials_list = [] #creates an empty list for user credentials
 
+    def save_credential(self):
+        '''
+        Test Case to check if the credentials object is saved in the credential list
+        '''
+        Password.credentials_list.append(self)
+
+
     def __init__(self, account_name, first_name, last_name, user_password):
         self.account_name = account_name
         self.first_name = first_name
         self.last_name = last_name
-        self.user_credentials = user_password
+        self.user_password = user_password
+
+    
 
 
